0a345cb8ba25d2bb5917a81330d97364e1cbebd9
Lambert Green <lambert.green@msn.com>
2018-09-15 17:04:27 -0700

    Added tmux config

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# vim conf
 dotfiles=~/dev/my/dotfiles
 ln -s $dotfiles/.vimrc ~/.vimrc  
 
+# nvim conf
 nvim_config=.config/nvim
 mkdir ~/$nvim_config
 ln -s $dotfiles/$nvim_config/init.vm ~/$nvim_config/init.vm
+
+# tmux confg
+ln -s $dotfiles/.tmux.conf ~/.tmux.conf
+
