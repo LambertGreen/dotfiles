b4fc4415dde0c126429d545fd5dbd5a4ba09d80f
Lambert Green <lgreen@tableau.com>
2018-10-17 10:08:58 -0700

    Updating Windows setup script to create the user Vim directory and link the user VimRC file

--- setup.ps1
+++ setup.ps1
@@ -7,6 +7,13 @@ function make-link ($target, $link) {
     New-Item -Path $link -ItemType SymbolicLink -Value $target
 }
 
+# vim config
+mkdir $env:USERPROFILE\.vim\
+mkdir $env:USERPROFILE\.vim\backup
+mkdir $env:USERPROFILE\.vim\swap
+mkdir $env:USERPROFILE\.vim\undo
+make-link $scriptDir/.vimrc $env:USERPROFILE/_vimrc
+
 # nvim conf
 make-link $scriptDir/.config/nvim/init.vim $env:LOCALAPPDATA/nvim/init.vim  
 make-link $scriptDir/.config/nvim/ginit.vim $env:LOCALAPPDATA/nvim/ginit.vim  
