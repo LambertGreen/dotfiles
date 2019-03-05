ae50444e6db0c2cd6a801646dad8032e5a1ab881
Lambert Green <lambert_green@msn.com>
2019-03-05 11:05:51 -0800

    Minor setup file updates.

--- setup-linux.sh
+++ setup-linux.sh
@@ -3,4 +3,7 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # tmux confg
-ln -sf $scriptDir/.tmux-linux.conf ~/.tmux-linux.conf
+# No longer needed... all conf is in .tmux.conf
+# ln -sf $scriptDir/.tmux-linux.conf ~/.tmux-linux.conf
+
+# TODO: Need to install bunch of software using a package manager
