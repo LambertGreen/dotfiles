3d66557ca721314d5b002bbc2e32a644a7597cb0
Lambert Green <lgreen@tableau.com>
2019-07-12 16:31:08 -0700

    Powershell_profile: fixing Set-VsCmd to use x64.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -51,7 +51,7 @@ function Set-VsCmd
         return
     }
     pushd $targetDir
-    cmd /c "vcvarsall.bat&set" |
+    cmd /c "vcvarsall.bat x64 & set" |
     foreach {
       if ($_ -match "(.*?)=(.*)") {
         Set-Item -force -path "ENV:\$($matches[1])" -value "$($matches[2])"
