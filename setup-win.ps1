e95c7d38e861341eed8759697af6a88f02a9115a
Lambert Green <lambert_green@msn.com>
2019-12-16 10:23:41 -0800

    ConEmu config updates.

--- setup-win.ps1
+++ setup-win.ps1
@@ -55,3 +55,7 @@ function SymlinkFlake8() {
 function SymlinkSpacemacs() {
     make-link $scriptDir/spacemacs.el $env:HOMEPATH/.spacemacs
 }
+
+function SymlinkKeypirinha() {
+    make-link $scriptDir/Keypirinha/Keypirinha.ini $env:HOMEPATH/scoop/persist/keypirinha/portable/Profile/User/Keypirinha.ini
+}
