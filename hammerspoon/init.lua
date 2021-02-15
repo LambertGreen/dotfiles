f9a73fddc649884d93111e5ceac06645191ba904
Lambert Green <lambert_green@msn.com>
2021-02-15 12:55:14 -0800

    Refactoring init
    Moving some commented out code into respective script files.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,11 +6,6 @@ hs.logger.defaultLogLevel = "info"
 -- require('HyperKey')
 require('HyperMode')
 require('ConfigWatcher')
--- Update 1: Disabling the below to try out getting this functionality directly from Karabiner-elements.
--- The reason for this is that I found that it is taking Hammerspoon to long to register the
--- pressing of the Escape key i.e. I need faster interactivity.
--- Update 2: Enabling this again because Karabiner-elements requires installing a kernel driver, and I don't
---  want to install it on the company laptop. So I hope that this "slower" escape, will be ok: tradeoffs.
 require('ControlTapToEscape')
 require('ShiftToBrackets')
 require('ShowKeys')
@@ -18,42 +13,3 @@ require('AppWatcher')
 
 -- Alert whenever this config is loaded.
 hs.alert.show("Hammerspoon: config loaded")
-
-
--- TODO: Use the below chooser code for providing user a menu
---
--- local chooser = hs.chooser.new(function(choice)
---       hs.alert.show(choice['text'])
--- end)
-
--- chooser:choices({
---       {
---          ["text"] = "Alfred\n",
---          ["subText"] = "macOS only\n",
---       },
---       {
---          ["text"] = "Quicksilver\n",
---          ["subText"] = "macOS only\n",
---       },
---       {
---          ["text"] = "Hammerspoon\n",
---          ["subText"] = "macOS only\n",
---       },
---       {
---          ["text"] = "Emacs\n",
---          ["subText"] = "is everywhere :)\n",
---       },
--- })
--- chooser:show()
-
--- TODO: Use MenuBar to show status of "programming mode" so that one can leave programming mode off most of
--- the time and enable it just when needed, which will mean we don't run into the password field entry issues.
---
--- mb = hs.menubar.new(nil)
--- updateStatus = function(event)
---   if(event == "on") then
---     mb:setTitle("🔴")
---   elseif(event == "off") then
---     mb:setTitle("🟢")
---   end
--- end
