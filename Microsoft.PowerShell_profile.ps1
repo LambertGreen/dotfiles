a78efd68f120e0b129cf690b449b41565232e032
Lambert Green <lambert_green@msn.com>
2021-03-04 10:43:46 -0800

    Powershell: remove ConEmu refs
    We are no longer using ConEmu.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,28 +13,10 @@ Import-Module Get-ChildItemColor
 Set-Alias ll Get-ChildItemColor -option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
 
-
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
-}
-
-# Set ConEmu to display CWD in Window title
-# Note: This function is just an example of
-# the metod to update the prompt.  The prompt
-# is actually updated by the global:Prompt
-# function.  To make it effective for a particular
-# theme one needs to update the theme file directly
-# Example: WindowsPowerShell\Modules\oh-my-posh\2.0.225\Themes\Paradox.psm1
-function updateConEmuWindowTitleToShowCwd
-{
-  $prompt = & $GitPromptScriptBlock
-  if ($env:ConEmuANSI -eq "ON")
-  {
-    $prompt += "$([char]27)]2;`"$((Get-Location).Path)`"$([char]7)"
-  }
-  $prompt
 }
 
 function lgreen-set-visual-studio-env
