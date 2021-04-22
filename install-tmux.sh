315a4a03d16c8be5884cba0750e1b9ece6f6544f
Lambert Green <lambert_green@msn.com>
2021-04-22 15:15:30 -0700

    Adds install for Tmux

--- install-tmux.sh
+++ install-tmux.sh
@@ -3,6 +3,9 @@
 TMUX_VERSION="2.0"
 LIBEVENT_VERSION="2.0.21-stable"
 
+# ----------------
+# Install libevent
+# ----------------
 [ -d ~/packages ] || mkdir ~/packages; pushd ~/packages;
 wget https://github.com/downloads/libevent/libevent/libevent-$LIBEVENT_VERSION.tar.gz
 tar zxvf libevent-$LIBEVENT_VERSION.tar.gz
@@ -11,11 +14,13 @@ cd libevent-$LIBEVENT_VERSION
 make
 sudo make install
 
+# ----------------
+# Install Tmux
+# ----------------
 [ -d ~/packages ] || mkdir ~/packages; pushd ~/packages;
 wget http://downloads.sourceforge.net/tmux/tmux-$TMUX_VERSION.tar.gz
 tar zxvf tmux-$TMUX_VERSION.tar.gz
 cd tmux-$TMUX_VERSION
-#./configure CFLAGS="-I/usr/local/include -I/usr/local/include/ncurses" LDFLAGS="-L/usr/local/lib -L/usr/local/include/ncurses -L/usr/local/include"
 LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
 make
 sudo make install
