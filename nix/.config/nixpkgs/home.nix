2028a0eec658e14248cfb2a45c73dcccaf83a380
Lambert Green <lambert_green@msn.com>
2022-09-14 11:59:08 -0700

    Fix and update nix home-manager config
    1. Added a number of packages since we are moving to using
    Nix/Home-Manager as a more primary package manager on Linux.
    2. Fix (workaround) a Nix locale issue by installing glibcLocales and
    then updating env var to point this locale-archive.

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -1,20 +1,71 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "lgreen";
+  home.homeDirectory = "/home/tsi/lgreen";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
       url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
     }))
   ];
 
-  home.packages = [
-    pkgs.fd
-    pkgs.tmux
-  ];
+  home = {
+    packages = with pkgs; [
+      git
+      stow
+      tmux
+      zsh
+      curl
+      keychain
+      aspell
+      binutils
+      ripgrep
+      tree
+      fd
+      bat
+      tldr
+      neofetch
+      direnv
+      (python39.withPackages (ps: with ps; [
+        pip
+        pynvim
+      ]))
+      (nerdfonts.override { fonts = [ "Iosevka" ]; })
+      glibcLocales
+    ];
+  };
 
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacsNativeComp;
-    extraPackages = (epkgs: [ epkgs.vterm ] );
+  programs = {
+    home-manager.enable = true;
+    jq.enable = true;
+    bat.enable = true;
+    command-not-found.enable = true;
+    dircolors.enable = true;
+    htop.enable = true;
+    info.enable = true;
+    exa.enable = true;
+
+    emacs = {
+      enable = true;
+      package = pkgs.emacsNativeComp;
+      extraPackages = (epkgs: [ epkgs.vterm ] );
+    };
+  };
+
+  fonts = {
+    fontconfig.enable = true;
   };
 }
