045854b5a39efab5f0de92fe4e120422e7734492
Lambert Green <lambert_green@msn.com>
2023-05-29 13:31:08 -0700

    Use catpuccin theme in Wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,15 @@
 local wezterm = require 'wezterm'
-local config = {}
 
-config.font = wezterm.font 'Iosevka NF'
+function scheme_for_appearance(appearance)
+	if appearance:find "Dark" then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
 
-return config
+return {
+	font = wezterm.font 'Iosevka NF',
+	hide_tab_bar_if_only_one_tab = true,
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+}
