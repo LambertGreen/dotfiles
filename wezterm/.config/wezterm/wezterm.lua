7b8ce7c53a877b1b18292dfd0170bd8379cea286
Lambert Green <lambert_green@msn.com>
2023-06-15 21:10:17 -0700

    Unify keybindings between tmux and wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -40,4 +40,34 @@ elseif wezterm.target_triple:find('apple') then
 	config.window_decorations = "TITLE | RESIZE | MACOS_FORCE_ENABLE_SHADOW"
 end
 
+config.leader = { key = 'a', mods = "CTRL|ALT" }
+config.keys = {
+	{ key = "\"", mods = "LEADER", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+	{ key = "%",  mods = "LEADER", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+	{ key = "z",  mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "c",  mods = "LEADER", action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+	{ key = "o",  mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Next" } },
+	{ key = "h",  mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Left" } },
+	{ key = "j",  mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Down" } },
+	{ key = "k",  mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Up" } },
+	{ key = "l",  mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Right" } },
+	{ key = "H",  mods = "LEADER", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+	{ key = "J",  mods = "LEADER", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
+	{ key = "K",  mods = "LEADER", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+	{ key = "L",  mods = "LEADER", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
+	{ key = 'p',  mods = "LEADER", action = wezterm.action { ActivateTabRelative = -1 } },
+	{ key = 'n',  mods = "LEADER", action = wezterm.action { ActivateTabRelative = 1 } },
+	{ key = "1",  mods = "LEADER", action = wezterm.action { ActivateTab = 0 } },
+	{ key = "2",  mods = "LEADER", action = wezterm.action { ActivateTab = 1 } },
+	{ key = "3",  mods = "LEADER", action = wezterm.action { ActivateTab = 2 } },
+	{ key = "4",  mods = "LEADER", action = wezterm.action { ActivateTab = 3 } },
+	{ key = "5",  mods = "LEADER", action = wezterm.action { ActivateTab = 4 } },
+	{ key = "6",  mods = "LEADER", action = wezterm.action { ActivateTab = 5 } },
+	{ key = "7",  mods = "LEADER", action = wezterm.action { ActivateTab = 6 } },
+	{ key = "8",  mods = "LEADER", action = wezterm.action { ActivateTab = 7 } },
+	{ key = "9",  mods = "LEADER", action = wezterm.action { ActivateTab = 8 } },
+	{ key = "&",  mods = "LEADER", action = wezterm.action { CloseCurrentTab = { confirm = true } } },
+	{ key = "x",  mods = "LEADER", action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+}
+
 return config
