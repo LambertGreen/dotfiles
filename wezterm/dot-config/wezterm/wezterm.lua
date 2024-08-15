6c1ee7761facad6278dea7efdd8aa7c5eb5e857c
Lambert Green <lambert.green@gmail.com>
2024-08-14 22:58:22 -0700

    More tmux like keybinds

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -70,37 +70,42 @@ end
 -- Keybindings
 --
 config.enable_csi_u_key_encoding = true
-
-config.leader = { key = 'a', mods = "CTRL|ALT", timeout_milliseconds = 2000 }
+config.leader = { key = 'a', mods = "ALT", timeout_milliseconds = 2000 }
 config.keys = {
-	{ key = "f",  mods = "CMD|CTRL", action = "ToggleFullScreen" },
-	{ key = "t",  mods = "CMD|CTRL", action = wezterm.action_callback(ToggleTheme) },
-	{ key = "\"", mods = "LEADER",   action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
-	{ key = "%",  mods = "LEADER",   action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
-	{ key = "z",  mods = "LEADER",   action = "TogglePaneZoomState" },
-	{ key = "c",  mods = "LEADER",   action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
-	{ key = "o",  mods = "LEADER",   action = wezterm.action { ActivatePaneDirection = "Next" } },
-	{ key = "h",  mods = "LEADER",   action = wezterm.action { ActivatePaneDirection = "Left" } },
-	{ key = "j",  mods = "LEADER",   action = wezterm.action { ActivatePaneDirection = "Down" } },
-	{ key = "k",  mods = "LEADER",   action = wezterm.action { ActivatePaneDirection = "Up" } },
-	{ key = "l",  mods = "LEADER",   action = wezterm.action { ActivatePaneDirection = "Right" } },
-	{ key = "H",  mods = "LEADER",   action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
-	{ key = "J",  mods = "LEADER",   action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
-	{ key = "K",  mods = "LEADER",   action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
-	{ key = "L",  mods = "LEADER",   action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
-	{ key = 'p',  mods = "LEADER",   action = wezterm.action { ActivateTabRelative = -1 } },
-	{ key = 'n',  mods = "LEADER",   action = wezterm.action { ActivateTabRelative = 1 } },
-	{ key = "1",  mods = "LEADER",   action = wezterm.action { ActivateTab = 0 } },
-	{ key = "2",  mods = "LEADER",   action = wezterm.action { ActivateTab = 1 } },
-	{ key = "3",  mods = "LEADER",   action = wezterm.action { ActivateTab = 2 } },
-	{ key = "4",  mods = "LEADER",   action = wezterm.action { ActivateTab = 3 } },
-	{ key = "5",  mods = "LEADER",   action = wezterm.action { ActivateTab = 4 } },
-	{ key = "6",  mods = "LEADER",   action = wezterm.action { ActivateTab = 5 } },
-	{ key = "7",  mods = "LEADER",   action = wezterm.action { ActivateTab = 6 } },
-	{ key = "8",  mods = "LEADER",   action = wezterm.action { ActivateTab = 7 } },
-	{ key = "9",  mods = "LEADER",   action = wezterm.action { ActivateTab = 8 } },
-	{ key = "&",  mods = "LEADER",   action = wezterm.action { CloseCurrentTab = { confirm = true } } },
-	{ key = "x",  mods = "LEADER",   action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+   -- Enter copy mode with Leader+]
+   { key = '[', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
+
+   -- Full screen and theme toggle (keeping your existing ones)
+   { key = 'f', mods = 'LEADER', action = 'ToggleFullScreen' },
+   { key = 't', mods = 'LEADER', action = wezterm.action_callback(ToggleTheme) },
+
+   -- Splits
+   { key = '\"', mods = 'LEADER', action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+   { key = '%', mods = 'LEADER', action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+
+   -- Pane controls and navigation
+   { key = 'z', mods = 'LEADER', action = 'TogglePaneZoomState' },
+   { key = 'o', mods = 'LEADER', action = wezterm.action { ActivatePaneDirection = "Next" } },
+   { key = 'h', mods = 'LEADER', action = wezterm.action { ActivatePaneDirection = "Left" } },
+   { key = 'j', mods = 'LEADER', action = wezterm.action { ActivatePaneDirection = "Down" } },
+   { key = 'k', mods = 'LEADER', action = wezterm.action { ActivatePaneDirection = "Up" } },
+   { key = 'l', mods = 'LEADER', action = wezterm.action { ActivatePaneDirection = "Right" } },
+
+   -- Pane resizing
+   { key = 'H', mods = 'LEADER', action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+   { key = 'J', mods = 'LEADER', action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
+   { key = 'K', mods = 'LEADER', action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+   { key = 'L', mods = 'LEADER', action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
+
+   -- Tabs management
+   { key = "c", mods = "LEADER", action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+   { key = 'p', mods = 'LEADER', action = wezterm.action { ActivateTabRelative = -1 } },
+   { key = 'n', mods = 'LEADER', action = wezterm.action { ActivateTabRelative = 1 } },
+   { key = '1', mods = 'LEADER', action = wezterm.action { ActivateTab = 0 } },
+
+   -- Close actions
+   { key = '&', mods = 'LEADER', action = wezterm.action { CloseCurrentTab = { confirm = true } } },
+   { key = 'x', mods = 'LEADER', action = wezterm.action { CloseCurrentPane = { confirm = true } } },
 }
 
 return config
