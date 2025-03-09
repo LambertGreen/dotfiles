1a4d28a4bf351570dff1a3fdf28a1048b59f25a6
Lambert Green <lambert.green@gmail.com>
2025-03-08 16:10:11 -0800

    Adds local project spelling words

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((jinx-dir-local-words . "Aporetic Iosevka OneHalfDark OneHalfLight wezterm"))))
