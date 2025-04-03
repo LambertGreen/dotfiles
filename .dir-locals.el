42fa6eeefb0aa67173fe38e4d3f944fb236f243f
Lambert Green <lambert.green@gmail.com>
2025-04-03 14:43:53 -0700

    Spelling words update

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-dir-local-words . "Aporetic Iosevka OneHalfDark OneHalfLight wezterm"))))
+((nil . ((jinx-dir-local-words . "Aporetic Iosevka OneHalfDark OneHalfLight autosuggest autosuggestions fg fi fzf gitconfig tmux uname usr wezterm zinit zinit's zle zsh zshrc"))))
